8eb97cdb8b4c4a1f6793ad0e6e9840a6d76e9517
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-08-10 19:09:48 +0530

    minor UI improvements

--- app/src/main/java/com/example/amansingh/presentation/TopBar.kt
+++ app/src/main/java/com/example/amansingh/presentation/TopBar.kt
@@ -25,8 +25,10 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.amansingh.R
 
 
 
@@ -40,7 +42,7 @@ fun TopBar(
         scrollBehavior = scrollBehavior,
         modifier = modifier,
         colors = TopAppBarDefaults.topAppBarColors(
-            containerColor = Color.Transparent,
+            containerColor = colorResource(id = R.color.bottombar),
             scrolledContainerColor = Color.Transparent
         ),
         title = {
@@ -50,41 +52,6 @@ fun TopBar(
                     .height(100.dp),
                 verticalArrangement = Arrangement.Center
             ) {
-//                Row(
-//                    modifier = Modifier
-//                        .fillMaxWidth()
-//                        .height(60.dp)
-//                        .border(
-//                            width = 1.dp,
-//                            color = Color.White,
-//                            shape = RoundedCornerShape(30.dp) // Reduced for a more subtle effect
-//                        )
-//                        .padding(horizontal = 16.dp),
-//                    verticalAlignment = Alignment.CenterVertically
-//                ) {
-//                    Icon(
-//                        imageVector = Icons.Rounded.Search,
-//                        contentDescription = "Search",
-//                        tint = Color.White,
-//                        modifier = Modifier.size(28.dp)
-//                    )
-//
-//                    Spacer(modifier = Modifier.width(16.dp))
-//
-//                    Text(
-//                        text = "Trending new jobs",
-//                        fontSize = 18.sp,
-//                        color = Color.White,
-//                        modifier = Modifier.weight(1f)
-//                    )
-//
-//                    Icon(
-//                        imageVector = Icons.Rounded.Mic,
-//                        contentDescription = "Voice search",
-//                        tint = Color.White,
-//                        modifier = Modifier.size(28.dp)
-//                    )
-//                }
 
                 Spacer(modifier = Modifier.height(10.dp))
 
