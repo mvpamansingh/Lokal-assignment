411626f71ebe414ef9bb579c46c03b4f33ae6a6d
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-08-10 16:26:11 +0530

    revamped jobitem card with material 3 principle

--- app/src/main/java/com/example/amansingh/presentation/card design.kt
+++ app/src/main/java/com/example/amansingh/presentation/card design.kt
@@ -13,128 +13,40 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Call
+import androidx.compose.material.icons.filled.CurrencyRupee
 import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Work
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.amansingh.R
+import com.example.amansingh.data.api.model.Results
+import com.example.amansingh.data.room.JobEntity
 
-//@Composable
-//fun MyCard() {
-//    Card(
-//        shape = RoundedCornerShape(16.dp),
-//        colors = CardDefaults.cardColors(
-//            containerColor = Color.Yellow
-//        ),
-//        modifier = Modifier
-//            .padding(16.dp)
-//            .fillMaxWidth()
-//            .wrapContentHeight()
-//    ) {
-//        Column(
-//            modifier = Modifier
-//                .padding(16.dp)
-//        ) {
-//            Text(text = "Location", fontSize = 20.sp, fontWeight = FontWeight.Bold)
-//            Spacer(modifier = Modifier.height(8.dp))
-//            Text(text = "Job Title", fontSize = 18.sp)
-//            Spacer(modifier = Modifier.height(8.dp))
-//            Text(text = "Salary", fontSize = 18.sp)
-//            Spacer(modifier = Modifier.height(16.dp))
-//            Button(
-//                onClick = { /* Handle Read More click */ },
-//                modifier = Modifier.align(Alignment.End)
-//            ) {
-//                Text(text = "Read More")
-//            }
-//        }
-//    }
-//}
-
-
-//@Composable
-//fun MyCard() {
-//    Card(
-//        shape = RoundedCornerShape(16.dp),
-//        colors =CardDefaults.cardColors(Color(0xFFACB4BB)),
-//        modifier = Modifier
-//            .padding(16.dp)
-//            .fillMaxWidth()
-//            .wrapContentHeight()
-//    ) {
-//        Column(
-//            modifier = Modifier
-//                .padding(16.dp)
-//        ) {
-//            // Row for Location and Bookmark Icon
-//            Row(
-//                verticalAlignment = Alignment.CenterVertically,
-//                horizontalArrangement = Arrangement.SpaceBetween,
-//                modifier = Modifier.fillMaxWidth()
-//            ) {
-//                // Location Card
-//                Card(
-//                    shape = RoundedCornerShape(50),
-//                    colors =CardDefaults.cardColors(Color(0xFF90CAF9)),
-//                    modifier = Modifier.padding(end = 2.dp)
-//                ) {
-//                    Text(
-//                        text = "Location",
-//                        fontSize = 15.sp,
-//                        fontWeight = FontWeight.Medium,
-//                        modifier = Modifier.padding(horizontal = 16.dp, vertical = 7.dp)
-//                    )
-//                }
-//                // Bookmark Icon
-//                IconButton(
-//                    onClick = { /* Handle bookmark click */ },
-//                ) {
-//                    Icon(
-//                        imageVector = Icons.Default.Bookmark,
-//                        contentDescription = "Bookmark",
-//                        tint = Color.Black
-//                    )
-//                }
-//            }
-//
-//            Spacer(modifier = Modifier.height(11.dp))
-//            Text(text = "Job Title", fontSize = 18.sp)
-//            Spacer(modifier = Modifier.height(8.dp))
-//            Text(text = "Salary", fontSize = 18.sp)
-//            Spacer(modifier = Modifier.height(16.dp))
-//            Row(modifier =Modifier.fillMaxWidth(),verticalAlignment = Alignment.CenterVertically,
-//                horizontalArrangement = Arrangement.SpaceBetween )
-//            {
-//                Text(text = "mobile number")
-//
-//                Button(
-//                    onClick = { /* Handle Read More click */ },
-//
-//                    border = BorderStroke(2.dp, Color.Red)
-//                ) {
-//                    Text(text = "Read More")
-//                }
-//            }
-//
-//
-//
-//        }
-//    }
-//}
 
 @Composable
-fun MyCard() {
+fun MyCard(
+    modifier:Modifier= Modifier,
+    job: Results,
+    onInfoClicked: () -> Unit,
+    bookmarkScreenViewmodel: BookmarkScreenViewmodel
+) {
+
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
     Card(
         shape = RoundedCornerShape(16.dp),
-        colors = CardDefaults.cardColors(Color(0xFFACB4BB)),
+        colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.card)),
+        elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
         modifier = Modifier
             .padding(16.dp)
             .fillMaxWidth()
@@ -153,7 +65,7 @@ fun MyCard() {
                 // Location Card
                 Card(
                     shape = RoundedCornerShape(50),
-                    colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.card)),
+                    colors = CardDefaults.cardColors(containerColor = colorResource(id = R.color.fetch_background)),
                     modifier = Modifier.padding(end = 2.dp)
                 ) {
                     Row(
@@ -168,7 +80,7 @@ fun MyCard() {
                         )
                         Spacer(modifier = Modifier.width(4.dp))
                         Text(
-                            text = "Location",
+                            text = "${job.job_location_slug}",
                             fontSize = 15.sp,
                             fontWeight = FontWeight.Medium,
                         )
@@ -176,7 +88,17 @@ fun MyCard() {
                 }
                 // Bookmark Icon
                 IconButton(
-                    onClick = { /* Handle bookmark click */ },
+                    onClick = {
+                        val bookmark = JobEntity(
+                            jobRole = job.job_role,
+                            location = job.job_location_slug,
+                            salaryMin = job.salary_min,
+                            salaryMax = job.salary_max,
+                            contactNumber = job.whatsapp_no,
+                            id = job.id.toString()
+                        )
+                        bookmarkScreenViewmodel.addBookmark(bookmark)
+                    },
                 ) {
                     Icon(
                         imageVector = Icons.Default.Bookmark,
@@ -199,11 +121,23 @@ fun MyCard() {
                     modifier = Modifier.size(20.dp)
                 )
                 Spacer(modifier = Modifier.width(4.dp))
-                Text(text = "Job Title", fontSize = 18.sp)
+                Text(text = "${job.job_role}", fontSize = 18.sp)
             }
 
             Spacer(modifier = Modifier.height(8.dp))
-            Text(text = "Salary", fontSize = 18.sp)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Icon(
+                    imageVector = Icons.Default.CurrencyRupee,
+                    contentDescription = "Job Title",
+                    tint = Color.Black,
+                    modifier = Modifier.size(20.dp)
+                )
+                Text(text = ": ${job.salary_min} - ${job.salary_max}", fontSize = 18.sp)
+
+            }
             Spacer(modifier = Modifier.height(16.dp))
             // Row for Mobile Number and Call Icon
             Row(
@@ -221,11 +155,13 @@ fun MyCard() {
                         modifier = Modifier.size(20.dp)
                     )
                     Spacer(modifier = Modifier.width(4.dp))
-                    Text(text = "Mobile Number")
+                    Text(text = "${job.whatsapp_no}")
                 }
 
                 Button(
-                    onClick = { /* Handle Read More click */ },
+                    onClick = { onInfoClicked() },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = colorResource(id = R.color.fetch_background)),
                     border = BorderStroke(2.dp, Color.Red),
 
                 ) {
@@ -235,8 +171,4 @@ fun MyCard() {
         }
     }
 }
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MyCard()
-}
+
