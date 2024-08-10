b641c5f0bc1f82e305648de80fbc582ecce0985f
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-08-10 11:40:00 +0530

    Updated Network module- creating singelton instances of room , jobdao and job dao Impl

--- app/src/main/java/com/example/amansingh/di/NetworkModule.kt
+++ app/src/main/java/com/example/amansingh/di/NetworkModule.kt
@@ -1,9 +1,15 @@
 package com.example.amansingh.di
 
+import android.content.Context
+import androidx.room.Room
 import com.example.amansingh.api.JobListAPI
+import com.example.amansingh.data.room.AppDatabaseClass
+import com.example.amansingh.data.room.JobDao
+import com.example.amansingh.data.room.JobDaoImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
@@ -28,4 +34,37 @@ class NetworkModule {
         return retrofit.create(JobListAPI::class.java)
     }
 
+
+
+
+
+
+    @Provides
+    @Singleton
+    fun provideDatabase(@ApplicationContext appContext: Context): AppDatabaseClass
+    {
+        return Room.databaseBuilder(
+            appContext,
+            AppDatabaseClass::class.java,
+            "AppDatabaseClass"
+        ).build()
+    }
+
+
+    @Provides
+    @Singleton
+    fun provideNoteDao(appDatabaseClass: AppDatabaseClass):JobDao
+    {
+        return appDatabaseClass.jobDao()
+    }
+
+
+
+
+    @Provides
+    @Singleton
+    fun notesDaoIml(jobDao: JobDao):JobDaoImpl
+    {
+        return     JobDaoImpl(jobDao)
+    }
 }
