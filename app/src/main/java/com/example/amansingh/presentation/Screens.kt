6c460e437abae200a6bf70317a47f6f16b46f7bf
aman singh <108426562+mvpamansingh@users.noreply.github.com>
2024-08-10 08:22:46 +0530

    added detailscreen routes

--- app/src/main/java/com/example/amansingh/presentation/Screens.kt
+++ app/src/main/java/com/example/amansingh/presentation/Screens.kt
@@ -3,4 +3,5 @@ package com.example.amansingh.presentation
 sealed class Screens (val screen: String){
     data object JobScreen: Screens("jobs")
     data object BookmarksScreen: Screens("bookmarks")
+    data object DetailsScreen :Screens("jobDetails")
 }
